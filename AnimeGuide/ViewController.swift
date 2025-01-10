5305d0cb8e7e0fde39d38ec631ee7968bdd587e4
Barbara Barone <babi.barone@cora.com.br>
2025-01-09 23:12:56 -0300

    Initial Commit

---
+++ AnimeGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeGuide
+//
+//  Created by Barbara Barone on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
