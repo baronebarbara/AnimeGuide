7cedf675991c3453245f51dfb2935c07c0ce0da1
Barbara Barone <babi.barone@cora.com.br>
2025-01-10 23:26:47 -0300

    adiciona camada de networking

---
+++ AnimeGuide/Networking/ApiClient/ApiClient.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+enum ApiError: Error {
+    case notFound,
+         business,
+         invalidData
+}
+
+protocol ApiClientProtocol {
+    func fetch<T: Decodable>(request: RequestProtocol) async throws -> T
+}
+
+final class ApiClient: ApiClientProtocol {
+    private let urlSession: URLSessionProtocol
+    
+    init(urlSession: URLSessionProtocol = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func fetch<T: Decodable>(request: RequestProtocol) async throws -> T {
+        guard let url = URL(string: request.path) else {
+            throw ApiError.notFound
+        }
+        
+        let (data, response) = try await urlSession.data(from: url, delegate: nil)
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        if let resultObject = try? decoder.decode(T.self, from: data) {
+            return resultObject
+        } else {
+            throw ApiError.invalidData
+        }
+    }
+}
